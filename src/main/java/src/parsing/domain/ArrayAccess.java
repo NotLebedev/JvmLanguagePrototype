d5a27e488521a44a419fb72a844982b33133616f
NotLebedev <artem-itf@yandex.ru>
2018-09-19 12:19:24 +0300

    *Applied TypeMatcher to ArrayAccess *Added corresponding code to bytecode equality test

--- src/main/java/src/parsing/domain/ArrayAccess.java
+++ src/main/java/src/parsing/domain/ArrayAccess.java
@@ -7,6 +7,7 @@ import src.parsing.domain.exceptions.ArrayExpectedException;
 import src.parsing.domain.exceptions.IncompatibleTypesException;
 import src.parsing.domain.structure.ClassFactory;
 import src.parsing.domain.structure.interfaces.AbstractClass;
+import src.parsing.domain.utils.TypeMatcher;
 
 /**
  * Class describing access to arrays
@@ -26,10 +27,7 @@ public class ArrayAccess implements Value {
 
         this.array = array;
 
-        if(!index.getType().equals(INDEX_TYPE)) //TODO : unboxing and type cast
-            throw new IncompatibleTypesException();
-
-        this.index = index;
+        this.index = TypeMatcher.getInstance().match(INDEX_TYPE, index);
 
         if(this.array.getType().getJvmName().length() < 2) //If length of classname is less than 2 it can not be an array
             throw new ArrayExpectedException();
