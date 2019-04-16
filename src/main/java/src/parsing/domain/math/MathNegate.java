86b6a8b6b937e021d7f3a06ba44373a1c14a5ada
NotLebedev <artem-itf@yandex.ru>
2019-04-16 16:27:33 +0300

    +Added MathNegate

---
+++ src/main/java/src/parsing/domain/math/MathNegate.java
@@ -0,0 +1,53 @@
+package src.parsing.domain.math;
+
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+import src.parsing.domain.exceptions.NotBoxedTypeException;
+import src.parsing.domain.exceptions.OperatorCanNotBeAppliedException;
+import src.parsing.domain.interfaces.Value;
+import src.parsing.domain.structure.interfaces.AbstractClass;
+import src.parsing.domain.utils.TypeMatcher;
+
+/**
+ * @author NotLebedev
+ */
+public class MathNegate implements Value {
+
+    private Value value;
+    private AbstractClass type;
+
+    public MathNegate(Value value) throws OperatorCanNotBeAppliedException {
+
+        if(!MathUtils.getMathCompatible().contains(value.getType()))
+            throw new OperatorCanNotBeAppliedException("-", value.getType());
+
+        try {
+            this.type = TypeMatcher.getInstance().getUnboxed(value.getType());
+        } catch (NotBoxedTypeException e) {
+            this.type = value.getType();
+        }
+
+        this.value = value;
+
+    }
+
+    @Override
+    public void generateBytecode(MethodVisitor methodVisitor) {
+
+        value.generateBytecode(methodVisitor);
+
+        try {
+            TypeMatcher.getInstance().doStackBoxing(type, methodVisitor);
+        } catch (NotBoxedTypeException e) {
+            throw new IllegalStateException("Expected to be boxed", e);
+        }
+
+        methodVisitor.visitInsn(type.getOpcode(Opcodes.INEG));
+
+    }
+
+    @Override
+    public AbstractClass getType() {
+        return type;
+    }
+}
---
+++ src/main/java/src/parsing/visitors/UnaryMinusVisitor.java
@@ -0,0 +1,10 @@
+package src.parsing.visitors;
+
+import src.parsing.antlr4Gen.Root.RootBaseVisitor;
+import src.parsing.domain.interfaces.Value;
+
+/**
+ * @author NotLebedev
+ */
+public class UnaryMinusVisitor extends RootBaseVisitor<Value> {
+}
