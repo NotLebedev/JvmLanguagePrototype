c1ae4247624a45a2d2d0bb5fe1eaa864aa3e4738
NotLebedev <artem-itf@yandex.ru>
2019-04-25 18:57:56 +0300

    *Finished MathBinaryOperator (partialy and not tested)

--- src/main/java/src/compilation/domain/math/MathBinaryOperator.java
+++ src/main/java/src/compilation/domain/math/MathBinaryOperator.java
@@ -1,12 +1,15 @@
 package src.compilation.domain.math;
 
 import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
 import src.compilation.domain.exceptions.IncompatibleTypesException;
 import src.compilation.domain.exceptions.NotBoxedTypeException;
 import src.compilation.domain.exceptions.OperatorCanNotBeAppliedException;
 import src.compilation.domain.interfaces.Value;
 import src.compilation.domain.structure.interfaces.AbstractClass;
 import src.compilation.domain.utils.TypeMatcher;
+
+import java.util.ArrayList;
 
 /**
  * @author NotLebedev
@@ -17,6 +20,12 @@ public class MathBinaryOperator implements Value {
 
     private final Value firstOperand;
     private final Value secondOperand;
+    private final AbstractClass type;
+
+    private static final ArrayList<AbstractClass> boxed = MathUtils.getBoxed();
+    private static final ArrayList<AbstractClass> mathCompatible = MathUtils.getMathCompatible();
+
+    private static final TypeMatcher typeMatcher = TypeMatcher.getInstance();
 
     public MathBinaryOperator(Type operatorType, Value firstOperand, Value secondOperand) throws OperatorCanNotBeAppliedException, IncompatibleTypesException {
 
@@ -41,20 +50,43 @@ public class MathBinaryOperator implements Value {
             secondType = secondOperand.getType();
         }
 
-        if(!MathUtils.getMathCompatible().contains(firstType))
+        if(!mathCompatible.contains(firstType))
             throw new OperatorCanNotBeAppliedException(operatorType.name, firstType);
 
-        if(!MathUtils.getMathCompatible().contains(secondType))
+        if(!mathCompatible.contains(secondType))
             throw new OperatorCanNotBeAppliedException(operatorType.name, secondType);
 
         if(!firstType.equals(secondType))
             throw new IncompatibleTypesException(firstType.getName(), secondType.getName());
+
+        this.type = firstType;
 
     }
 
     @Override
     public void generateBytecode(MethodVisitor methodVisitor) {
 
+        firstOperand.generateBytecode(methodVisitor);
+        unBox(firstOperand.getType(), methodVisitor);
+
+        secondOperand.generateBytecode(methodVisitor);
+        unBox(secondOperand.getType(), methodVisitor);
+
+        methodVisitor.visitInsn(type.getOpcode(operatorType.opcode));
+
+    }
+
+    private void unBox(AbstractClass type, MethodVisitor methodVisitor) {
+
+        if(boxed.contains(type)) {
+
+            try {
+                typeMatcher.doStackBoxing(type, methodVisitor);
+            } catch (NotBoxedTypeException e) {
+                throw new IllegalStateException("Expected to be boxed", e);
+            }
+
+        }
     }
 
     @Override
@@ -64,14 +96,16 @@ public class MathBinaryOperator implements Value {
 
     public enum Type {
 
-        MULTIPLY("*"),
-        DIVIDE("/"),
-        REMAINDER("%");
+        MULTIPLY("*", Opcodes.IMUL),
+        DIVIDE("/", Opcodes.IDIV),
+        REMAINDER("%", Opcodes.IREM);
 
         public final String name;
+        public final int opcode;
 
-        Type(String name) {
+        Type(String name, int opcode) {
             this.name = name;
+            this.opcode = opcode;
         }
     }
 
