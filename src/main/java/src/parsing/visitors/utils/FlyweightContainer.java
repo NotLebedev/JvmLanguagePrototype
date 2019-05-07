bbc9984eafccbe95b67888e291b22192b1170b30
NotLebedev <artem-itf@yandex.ru>
2019-05-07 15:10:05 +0300

    -Removed MultiKeyHashMap

--- src/main/java/src/parsing/visitors/utils/FlyweightContainer.java
+++ src/main/java/src/parsing/visitors/utils/FlyweightContainer.java
@@ -2,9 +2,7 @@ package src.parsing.visitors.utils;
 
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
 
-import java.lang.ref.WeakReference;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -16,31 +14,18 @@ import java.util.stream.Collectors;
  */
 public class FlyweightContainer<T> {
 
-    private final MultiKeyHashMap<WeakReference<T>> flyweightMap;
     private final Cache<List<Integer>, T> flyweightCache;
     private final Class<?>[] keyTypes;
 
     public FlyweightContainer(Class<?>... keyTypes) {
 
         this.keyTypes = keyTypes;
-        this.flyweightMap = new MultiKeyHashMap<>(keyTypes);
         this.flyweightCache = CacheBuilder.newBuilder().weakValues().build();
 
     }
 
     public T getFlyweight(Supplier<T> supplier, Object... keys) throws InvalidKeyTypesException {
 
-        /*WeakReference<T> result = flyweightMap.get(keys);
-
-        if(result != null)
-            return result.get();
-        else {
-
-            T flyweight = supplier.get();
-            flyweightMap.put(new WeakReference<>(flyweight), keys);
-            return flyweight;
-
-        }*/
         verifyKeys(keys);
         try {
             return flyweightCache.get(Arrays.stream(keys).map(Object::hashCode).collect(Collectors.toList()), supplier::get);
