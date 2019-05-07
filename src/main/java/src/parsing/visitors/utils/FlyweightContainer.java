9cb35d43f56631f3054374fc03f6ff8e8e04e362
NotLebedev <artem-itf@yandex.ru>
2019-05-07 18:09:00 +0300

    *Changed Cache to use objects as keys instead of their hashes, to prevent memory leak made keys weak referenced too +Added documentation to FlyweightContainer

--- src/main/java/src/parsing/visitors/utils/FlyweightContainer.java
+++ src/main/java/src/parsing/visitors/utils/FlyweightContainer.java
@@ -7,30 +7,46 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 /**
+ * Class used to store flyweights (currently only different tree visitors)
+ * it stores all values and keys weakly to prevent memory leaks
  * @author NotLebedev
  */
 public class FlyweightContainer<T> {
 
-    private final Cache<List<Integer>, T> flyweightCache;
+    private final Cache<List<Object>, T> flyweightCache;
     private final Class<?>[] keyTypes;
 
+    /**
+     * @param keyTypes types of keys to be associated with objects
+     */
     public FlyweightContainer(Class<?>... keyTypes) {
 
         this.keyTypes = keyTypes;
-        this.flyweightCache = CacheBuilder.newBuilder().weakValues().build();
+        this.flyweightCache = CacheBuilder.newBuilder()
+                .weakValues()//Values to be released as soon, as tree is visited
+                .weakKeys() //Since on of the keys is Method which holds whole compilation tree
+                .build();   //keys should also be weak referenced to be released as code is compiled
 
     }
 
+    /**
+     * Get an instance of flyweight T
+     * @param supplier supplier of a new T object in case no object with given keys is stored
+     * @param keys keys associated with required object T
+     * @return T object associated with given keys
+     * @throws InvalidKeyTypesException key types given do not match key types specified on
+     * construction
+     */
     public T getFlyweight(Supplier<T> supplier, Object... keys) throws InvalidKeyTypesException {
 
         verifyKeys(keys);
+
         try {
-            return flyweightCache.get(Arrays.stream(keys).map(Object::hashCode).collect(Collectors.toList()), supplier::get);
+            return flyweightCache.get(Arrays.asList(keys), supplier::get);
         } catch (ExecutionException e) {
-            throw new IllegalStateException("flyweight cache failed");
+            throw new IllegalStateException("Flyweight cache failed");
         }
 
     }
