4c1da97d7a0accb516a0c563ffd42108b745fbc7
NotLebedev <artem-itf@yandex.ru>
2019-06-08 19:01:25 +0300

    *Partially implemented RelationLogicOperatorVisitor

--- src/main/java/src/parsing/visitors/methodCodeVisitors/ValueVisitors/RelationLogicOperatorVisitor.java
+++ src/main/java/src/parsing/visitors/methodCodeVisitors/ValueVisitors/RelationLogicOperatorVisitor.java
@@ -1,11 +1,16 @@
 package src.parsing.visitors.methodCodeVisitors.ValueVisitors;
 
+import src.compilation.domain.exceptions.IncompatibleTypesException;
+import src.compilation.domain.exceptions.OperatorCanNotBeAppliedException;
 import src.compilation.domain.interfaces.Scope;
 import src.compilation.domain.interfaces.Value;
 import src.compilation.domain.logic.LogicRelationOperator;
 import src.parsing.antlr4Gen.Root.RootBaseVisitor;
 import src.parsing.antlr4Gen.Root.RootParser;
 import src.parsing.visitors.errorHandling.ErrorCollector;
+import src.parsing.visitors.errorHandling.errors.IncompatibleTypesError;
+import src.parsing.visitors.errorHandling.errors.OperatorCanNotBeAppliedError;
+import src.parsing.visitors.errorHandling.exceptions.ExpressionParseCancellationException;
 import src.parsing.visitors.utils.FlyweightContainer;
 import src.parsing.visitors.utils.InvalidKeyTypesException;
 
@@ -38,15 +43,57 @@ public class RelationLogicOperatorVisitor extends RootBaseVisitor<Value> {
 
     }
 
+    @SuppressWarnings("Duplicates")
     @Override
     public Value visitRELATIONAL_OP(RootParser.RELATIONAL_OPContext ctx) {
+
+        //Acquire both values
+        var valueVisitor = ValueVisitor.getInstance(scope, errorCollector);
+
+        Value val1 = ctx.value(0).accept(valueVisitor);
+        Value val2 = ctx.value(1).accept(valueVisitor);
+
+        throw new IllegalStateException("Not implemented yet");
+
+    }
+
+    @SuppressWarnings("Duplicates")
+    @Override
+    public Value visitEQUALITY_OP(RootParser.EQUALITY_OPContext ctx) {
+
+        //Acquire both values
+        var valueVisitor = ValueVisitor.getInstance(scope, errorCollector);
+
+        Value val1 = ctx.value(0).accept(valueVisitor);
+        Value val2 = ctx.value(1).accept(valueVisitor);
+
         throw new IllegalStateException("Not implemented yet");
     }
 
-    @Override
-    public Value visitEQUALITY_OP(RootParser.EQUALITY_OPContext ctx) {
-        throw new IllegalStateException("Not implemented yet");
+    private Value buildValue(Value val1, Value val2, LogicRelationOperator.Type operationType,
+                             int line, int charPosition, String offendingSymbol) {
+
+        try { //Try creating operator
+            return new LogicRelationOperator(operationType, val1, val2);
+        } catch (OperatorCanNotBeAppliedException e) { //Operator can not be applied to one of the types
+            errorCollector.reportError(new OperatorCanNotBeAppliedError(
+                    line, charPosition,
+                    offendingSymbol,
+                    "Operator '" + operationType.name + "' can not be applied to types " +
+                            val1.getType().getName() + " and " + val2.getType().getName()
+            ));
+            throw new ExpressionParseCancellationException();
+        } catch (IncompatibleTypesException e) { //Two types cant be used together
+            errorCollector.reportError(new IncompatibleTypesError(
+                    line, charPosition,
+                    offendingSymbol,
+                    val1.getType().getName(), val2.getType().getName()
+            ));
+            throw new ExpressionParseCancellationException();
+        }
+
     }
+
 
     private LogicRelationOperator.Type getOperationType(RootParser.RELATIONAL_OPContext ctx) {
 
