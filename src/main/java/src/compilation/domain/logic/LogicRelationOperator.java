8b388b5a95422ca29fa5f842ed3e88d8508fdf14
NotLebedev <artem-itf@yandex.ru>
2019-06-08 12:41:22 +0300

    +Added enum with operator types to LogicRelationOperator +Added class fields and constructor

--- src/main/java/src/compilation/domain/logic/LogicRelationOperator.java
+++ src/main/java/src/compilation/domain/logic/LogicRelationOperator.java
@@ -2,10 +2,26 @@ package src.compilation.domain.logic;
 
 import org.objectweb.asm.MethodVisitor;
 import src.compilation.domain.interfaces.Value;
+import src.compilation.domain.structure.ClassFactory;
 import src.compilation.domain.structure.interfaces.AbstractClass;
 
 public class LogicRelationOperator implements Value {
 
+    private final Type operatorType;
+
+    private final Value firstOperand;
+    private final Value secondOperand;
+
+    private static final AbstractClass type =
+            ClassFactory.getInstance().forCorrectName("boolean");
+
+    public LogicRelationOperator(Type operatorType, Value firstOperand, Value secondOperand) {
+
+        this.operatorType = operatorType;
+        this.firstOperand = firstOperand;
+        this.secondOperand = secondOperand;
+
+    }
 
     @Override
     public void generateBytecode(MethodVisitor methodVisitor) {
@@ -14,6 +30,19 @@ public class LogicRelationOperator implements Value {
 
     @Override
     public AbstractClass getType() {
-        return null;
+        return type;
     }
+
+    public enum Type {
+
+        LESS,
+        LESS_EQUAL,
+        GREATER,
+        GREATER_EQUAL,
+
+        EQUAL,
+        NOT_EQUAL
+
+    }
+
 }
